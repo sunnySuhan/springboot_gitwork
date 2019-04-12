99f8ad890e27f7e38785a1a82cf5c77a219575ee
sgh <sugh029@163.com>
2019-04-12 19:25:07 +0800

    init version

---
+++ src/main/java/com/sgh/SpringbootGitworkApplication.java
@@ -0,0 +1,13 @@
+package com.sgh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGitworkApplication.class, args);
+    }
+
+}
